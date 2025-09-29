622f37a28adc307d9184ba768f250977194257fe
Shoky <leozul@hotmail.es>
2025-09-29 19:42:44 +0200

    Added privacy and terms of service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { TermsOfService } from './terms-of-service/terms-of-service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // ... otras rutas existentes
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'terms-of-service', component: TermsOfService },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { HowItWorksComponent } from './how-it-works/how-it-works';
 import { Footer } from './footer/footer';
 import { Donation } from './donation/donation';
 import { Features } from './features/features';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,HowItWorksComponent,Footer,Donation,Features],
+  imports: [HeaderComponent, HowItWorksComponent, Footer, Donation, Features, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
